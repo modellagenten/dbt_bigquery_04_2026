61baee3fda546433f9a106632d1be55813a958bb
Maximilian Brell <maximilian.brell@ing.de>
2026-04-28 12:07:39 +0000

    implemented group C tasks

---
+++ models/marts/core/dim_products_priced.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table') }}
+with src as (
+ select * from {{ ref('stg_products_cents') }}
+)
+select
+ product_id,
+ product_name,
+ brand,
+ category,
+ {{ cents_to_euros('retail_price_cents') }} as
+retail_price_eur,
+ {{ cents_to_euros('cost_cents') }} as
+cost_eur,
+ {{ cents_to_euros('retail_price_cents - cost_cents', precision=4) }}
+as margin_eur
+from src
