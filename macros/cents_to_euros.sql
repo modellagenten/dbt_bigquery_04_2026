60bdb57bac189dfb1c57687981dacff0259e6ecd
Maximilian Brell <maximilian.brell@ing.de>
2026-04-28 13:03:45 +0000

    formatting

--- macros/cents_to_euros.sql
+++ macros/cents_to_euros.sql
@@ -4,5 +4,5 @@
 100.0, 2)
 #}
 {% macro cents_to_euros(column_name, precision=2) %}
- round(({{ column_name }}) / 100.0, {{ precision }})
+    round(({{ column_name }}) / 100.0, {{ precision }})
 {% endmacro %}
--- models/marts/core/dim_products_priced.sql
+++ models/marts/core/dim_products_priced.sql
@@ -1,18 +1,13 @@
-{{ config(materialized='table') }}
-with src as (
- select * from {{ ref('stg_products_cents') }}
-)
+{{ config(materialized="table") }}
+with src as (select * from {{ ref("stg_products_cents") }})
 select
- product_id,
- product_name,
- brand,
- category,
- {{ cents_to_euros('retail_price_cents') }} as
-retail_price_eur,
- {{ cents_to_euros('cost_cents') }} as
-cost_eur,
- {{ cents_to_euros('retail_price_cents - cost_cents', precision=4) }}
-as margin_eur,
-retail_price_cents,
-cost_cents
+    product_id,
+    product_name,
+    brand,
+    category,
+    {{ cents_to_euros("retail_price_cents") }} as retail_price_eur,
+    {{ cents_to_euros("cost_cents") }} as cost_eur,
+    {{ cents_to_euros("retail_price_cents - cost_cents", precision=4) }} as margin_eur,
+    retail_price_cents,
+    cost_cents
 from src
